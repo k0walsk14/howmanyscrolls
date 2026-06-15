3fcde6df5ca65ec07bb0f37b965b4807b53c8fe1
Dominik Kowaluk <kowaluk.dominik@gmail.com>
2026-06-15 15:58:57 +0200

    First commit: Discord bot script that live counts if a person sends tiktoks/reels and adds them to a permanent  database for later viewing, also possible to count all tiktoks/reels sent to a specific channel ever

---
+++ database.py
@@ -0,0 +1,29 @@
+import sqlite3
+import json
+
+def reset_db():
+    with open('counts.json', 'r') as file:
+        old_data = json.load(file)
+
+    blank_data = {
+        "tiktok": 0,
+        "reel": 0
+    }
+    with open('counts.json', 'w') as file:
+        json.dump(blank_data, file, indent=4)
+    
+    return old_data
+
+#platform because we can execute the function with either platform == 'tiktok' or 'reel'
+def increment_count(platform):
+
+    #open the db file and get current scores from there to RAM
+    with open('counts.json', 'r') as file:
+        data = json.load(file)
+
+    #update the score with what discordbot has counted
+    data[platform] +=1
+
+    #save the appended count back to the file so that it stays permanent
+    with open('counts.json', 'w') as file:
+        json.dump(data, file, indent=4)
---
+++ howmanyscrolls.py
@@ -0,0 +1,68 @@
+import discord
+import os
+import database
+from dotenv import load_dotenv
+load_dotenv()
+
+class MyClient(discord.Client):
+    #ja
+    admin_id = int(os.getenv('ADMIN_ID'))
+    #kuba
+    target_id = int(os.getenv('TARGET_ID'))
+    links_channel_id = int(os.getenv('LINKS_CHANNEL_ID'))
+
+    async def on_ready(self):
+        print(f'Hi im {self.user}, Im ready to count the scrolls!')
+
+    async def on_message(self, message):
+        if self.target_id == message.author.id:
+            #getting rid of capitalization issues by making all message content lowercap
+            content_lower = message.content.lower()
+            tiktok = 'tiktok.com'
+            reel = 'instagram.com'
+            if tiktok in content_lower:
+                print(f"{tiktok} found adding +1 tiktok to the scrolling grind")
+                #executing the database.py so that we permanently save the data about the tiktok count 
+                database.increment_count('tiktok')
+            if reel in content_lower:
+                print(f"{reel} found adding +1 reel to the scrolling grind")
+                #executing the database.py so that we permanently save the data about the reel count
+                database.increment_count('reel')
+
+        if self.admin_id == message.author.id:
+            content_lower = message.content.lower()
+            if message.content == '!sync':
+                print("deleting the db data so that we wont get double count while syncing")
+                deleted_data = database.reset_db()
+
+                sync_tiktoks = 0
+                sync_reels = 0
+                target_channel = self.get_channel(self.links_channel_id)
+
+                print("Starting historical sync... (scanning the whole discord for tt/reels links and counting them)")
+                async for old_msg in target_channel.history(limit=None):
+                    if self.target_id == old_msg.author.id:
+                        old_content_lower = old_msg.content.lower()
+                    #getting rid of capitalization issues by making all message content lowercap
+                        tiktok = 'tiktok.com'
+                        reel = 'instagram.com'
+
+                        if tiktok in old_content_lower:
+                        #executing the database.py so that we permanently save the data about the tiktok count 
+                            database.increment_count('tiktok')
+                            sync_tiktoks += 1
+
+                        if reel in old_content_lower:
+                        #executing the database.py so that we permanently save the data about the reel count
+                            database.increment_count('reel')
+                            sync_reels += 1
+                print(f"deleted {deleted_data} during the !sync")
+                print(f"Successfully found and counted {sync_tiktoks} tiktoks and {sync_reels} reels")
+
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(os.getenv('DISCORD_TOKEN'))
